a252c486abf650dc9b84778d005edae888c03a26
Supinic <supinic@protonmail.com>
2024-05-07 22:02:58 +0200

    wrap main body in async IIFE

--- index.js
+++ index.js
@@ -23,45 +23,47 @@ const archiveUrl = async (url) => {
 
 require("./db-creds.js");
 
-const core = await import("supi-core");
-const Query = new core.Query({
-	user: process.env.MARIA_USER,
-	password: process.env.MARIA_PASSWORD,
-	host: process.env.MARIA_HOST,
-	connectionLimit: process.env.MARIA_CONNECTION_LIMIT
-});
+(async () => {
+	const core = await import("supi-core");
+	const Query = new core.Query({
+		user: process.env.MARIA_USER,
+		password: process.env.MARIA_PASSWORD,
+		host: process.env.MARIA_HOST,
+		connectionLimit: process.env.MARIA_CONNECTION_LIMIT
+	});
 
-const configData = await Query.getRecordset(rs => rs
-	.select("*")
-	.from("data", "Config"));
+	const configData = await Query.getRecordset(rs => rs
+		.select("*")
+		.from("data", "Config"));
 
-core.Config.load(configData);
+	core.Config.load(configData);
 
-const job = new CronJob("0 0 0 * * 1", async () => {
-	const originNotes = await core.Query.getRecordset(rs => rs
-		.select("Notes")
-		.from("data", "Origin")
-		.where("Notes %*like* OR Notes %*like*", "http://", "https://")
-		.flat("Notes"));
-	const suggestionTexts = await core.Query.getRecordset(rs => rs
-		.select("Text")
-		.from("data", "Suggestion")
-		.where("Text %*like* OR Text %*like*", "http://", "https://")
-		.flat("Text"));
+	const job = new CronJob("0 0 0 * * 1", async () => {
+		const originNotes = await core.Query.getRecordset(rs => rs
+			.select("Notes")
+			.from("data", "Origin")
+			.where("Notes %*like* OR Notes %*like*", "http://", "https://")
+			.flat("Notes"));
+		const suggestionTexts = await core.Query.getRecordset(rs => rs
+			.select("Text")
+			.from("data", "Suggestion")
+			.where("Text %*like* OR Text %*like*", "http://", "https://")
+			.flat("Text"));
 
-	const data = [
-		...originNotes, ...suggestionTexts
-	];
+		const data = [
+			...originNotes, ...suggestionTexts
+		];
 
-	const regex = /(https?:\/\/.+?)(\s|$)/g;
-	for (const note of data) {
-		for (const match of note.matchAll(regex)) {
-			await archiveUrl(match[1]);
-			await setTimeout(6000);
+		const regex = /(https?:\/\/.+?)(\s|$)/g;
+		for (const note of data) {
+			for (const match of note.matchAll(regex)) {
+				await archiveUrl(match[1]);
+				await setTimeout(6000);
+			}
 		}
-	}
-});
+	});
 
-job.start();
+	job.start();
 
-globalThis.job = { job };
+	globalThis.job = { job };
+})();
