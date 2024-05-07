672c53c0c7d2acd00bca4f95e3639fa2b94fc9bf
Supinic <supinic@protonmail.com>
2024-05-07 21:58:40 +0200

    fixing up stuff

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const { setTimeout } = require("node:timers/promises");
+const { CronJob } = require("cron");
 const archiveUrl = async (url) => {
+	const accessKey = core.Config.get("API_INTERNET_ARCHIVE_ACCESS_KEY");
+	const secretKey = core.Config.get("API_INTERNET_ARCHIVE_SECRET_KEY");
+
 	await fetch("https://web.archive.org/save", {
 		method: "POST",
 		headers: {
 			Accept: "application/json",
 			"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-			Authorization: `LOW ${sb.Config.get("API_INTERNET_ARCHIVE_ACCESS_KEY")}:${sb.Config.get("API_INTERNET_ARCHIVE_SECRET_KEY")}`
+			Authorization: `LOW ${accessKey}:${secretKey}`
 		},
 		body: new URLSearchParams({
 			url,
-			capture_screenshot: "on",
 			if_not_archived_within: "7d",
+			capture_screenshot: "on",
 			skip_first_archive: "on",
 			delay_wb_availability: "on"
 		}).toString()
 	});
 }
+
+require("./db-creds.js");
 
 const core = await import("supi-core");
 const Query = new core.Query({
@@ -31,35 +37,31 @@ const configData = await Query.getRecordset(rs => rs
 
 core.Config.load(configData);
 
-export const definition = {
-	name: "web-archive-link-archiver",
-	expression: "0 0 0 * * 1",
-	description: "Archives all links found in the Origin and Suggestion tables via Internet Archive",
-	code: (async function archiveTrainsTweets () {
-		const originNotes = await core.Query.getRecordset(rs => rs
-			.select("Notes")
-			.from("data", "Origin")
-			.where("Notes %*like* OR Notes %*like*", "http://", "https://")
-			.flat("Notes")
-		);
-		const suggestionTexts = await core.Query.getRecordset(rs => rs
-			.select("Text")
-			.from("data", "Suggestion")
-			.where("Text %*like* OR Text %*like*", "http://", "https://")
-			.flat("Text")
-		);
+const job = new CronJob("0 0 0 * * 1", async () => {
+	const originNotes = await core.Query.getRecordset(rs => rs
+		.select("Notes")
+		.from("data", "Origin")
+		.where("Notes %*like* OR Notes %*like*", "http://", "https://")
+		.flat("Notes"));
+	const suggestionTexts = await core.Query.getRecordset(rs => rs
+		.select("Text")
+		.from("data", "Suggestion")
+		.where("Text %*like* OR Text %*like*", "http://", "https://")
+		.flat("Text"));
 
-		const data = [
-			...originNotes,
-			...suggestionTexts,
-		];
+	const data = [
+		...originNotes, ...suggestionTexts
+	];
 
-		const regex = /(https?:\/\/.+?)(\s|$)/g;
-		for (const note of data) {
-			for (const match of note.matchAll(regex)) {
-				await archiveUrl(match[1]);
-				await setTimeout(6000);
-			}
+	const regex = /(https?:\/\/.+?)(\s|$)/g;
+	for (const note of data) {
+		for (const match of note.matchAll(regex)) {
+			await archiveUrl(match[1]);
+			await setTimeout(6000);
 		}
-	})
-};
+	}
+});
+
+job.start();
+
+globalThis.job = { job };
